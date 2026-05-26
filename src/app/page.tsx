b00e18ad34616364626420dc1d602509e1d455f3
PhamThanhSu <psu95228@gmail.com>
2026-05-27 06:36:36 +0700

    Show landing page at root path (/) by rendering LandingPage in app/page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import ClientLayout from "@/app/(client)/layout";
+import LandingPage from "@/app/(client)/landing/page";
+
+export default function Home() {
+  return (
+    <ClientLayout>
+      <LandingPage />
+    </ClientLayout>
+  );
+}
